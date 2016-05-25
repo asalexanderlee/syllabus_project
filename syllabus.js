220b531e9cb983685568e55d6f0414bdffb7833a
asalexanderlee <asalexanderlee@davidson.edu>
2016-05-25 16:38:58 -0400

    Update syllabus.js

--- syllabus.js
+++ syllabus.js
@@ -11,7 +11,19 @@ $(document).ready(function(){
   
   $("#prof").click(function(){
     $("#prof_list").toggle();
-    $("#down_arrow").toggle();
-    $("#side_arrow").toggle();
+    $("#down_arrow_prof").toggle();
+    $("#side_arrow_prof").toggle();
   });
+  
+  $("#dept").click(function(){
+    $("#dept_list").toggle();
+    $("#down_arrow_dept").toggle();
+    $("#side_arrow_dept").toggle();
+  });
+  
+  $("#class_size").click(function(){
+    $("#class_size_list").toggle();
+    $("#down_arrow_class_size").toggle();
+    $("#side_arrow_class_size").toggle();
+  }); 
 });
