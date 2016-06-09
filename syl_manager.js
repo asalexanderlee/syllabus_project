751dc90d8039ee6785e13509224201c56791876b
asalexanderlee <asalexanderlee@davidson.edu>
2016-06-09 15:40:25 -0400

    Add files via upload

---
+++ syl_manager.js
@@ -0,0 +1,10 @@
+function shareSylInfo(){
+				document.getElementById("syl_status_box").style.visibility = "visible";	
+			}
+			function shareProfInfo(){
+				document.getElementById("prof_search").style.visibility = "visible";
+				document.getElementById("only_missing_prof").style.visibility = "visible";	
+			}
+			function hideStatBox(){
+				document.getElementById("syl_status_box").style.visibility = "hidden";	
+			}
